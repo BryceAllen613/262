b07331c22b00e334e7e23e2764c8083b81eb75fd
bda5 <bda5@gold26.cs.calvin.edu>
2019-10-18 16:51:48 -0400

    lab 07

---
+++ Lab07/monopoly.sql
@@ -0,0 +1,71 @@
+﻿--
+-- This SQL script builds a monopoly database, deleting any pre-existing version.
+--
+-- @author kvlinden
+-- @version Summer, 2015
+--
+
+-- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
+DROP TABLE IF EXISTS PlayerGame;
+DROP TABLE IF EXISTS Game;
+DROP TABLE IF EXISTS Player;
+DROP TABLE IF EXISTS PlayerList;
+DROP TABLE IF EXISTS UnifinishedGame;
+
+-- Create the schema.
+CREATE TABLE Game (
+	ID integer PRIMARY KEY, 
+	time timestamp
+	);
+
+CREATE TABLE Player (
+	ID integer PRIMARY KEY, 
+	emailAddress varchar(50) NOT NULL,
+	name varchar(50)
+	);
+
+CREATE TABLE PlayerGame (
+	gameID integer REFERENCES Game(ID), 
+	playerID integer REFERENCES Player(ID),
+	score integer
+	);
+
+CREATE TABLE PlayerList (
+	ID integer PRIMARY KEY,
+	GameID integer REFERENCES Game(ID),
+	PlayerId integer REFERENCES Player(ID),
+	score integer REFERENCES PlayerGame(score)
+	);
+
+CREATE TABLE UnfinishedGame (
+	ID integer PRIMARY KEY,
+	PlayerListID integer REFERENCES PlayerList(ID),
+	time timeStamp
+	);
+
+-- Allow users to select data from the tables.
+GRANT SELECT ON Game TO PUBLIC;
+GRANT SELECT ON Player TO PUBLIC;
+GRANT SELECT ON PlayerGame TO PUBLIC;
+GRANT SELECT ON PlayerList TO PUBLIC;
+GRANT SELECT ON UnfininshedGame TO PUBLIC;
+-- Add sample records.
+INSERT INTO Game VALUES (1, '2006-06-27 08:00:00');
+INSERT INTO Game VALUES (2, '2006-06-28 13:20:00');
+INSERT INTO Game VALUES (3, '2006-06-29 18:41:00');
+
+INSERT INTO Player(ID, emailAddress) VALUES (1, 'me@calvin.edu');
+INSERT INTO Player VALUES (2, 'king@gmail.edu', 'The King');
+INSERT INTO Player VALUES (3, 'dog@gmail.edu', 'Dogbreath');
+
+INSERT INTO PlayerGame VALUES (1, 1, 0.00);
+INSERT INTO PlayerGame VALUES (1, 2, 0.00);
+INSERT INTO PlayerGame VALUES (1, 3, 2350.00);
+INSERT INTO PlayerGame VALUES (2, 1, 1000.00);
+INSERT INTO PlayerGame VALUES (2, 2, 0.00);
+INSERT INTO PlayerGame VALUES (2, 3, 500.00);
+INSERT INTO PlayerGame VALUES (3, 2, 0.00);
+INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
+
+INSERT INTO PlayerList VALUES (1, 2, 2, 1);
+INSERT INTO UnfinishedGame VALUES (1,1,'2019-10-18 08:00:00');
